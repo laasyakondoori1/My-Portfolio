c11b33ed83a90861bc1eed388d0579898c8d7322
Laasya <laasyakondoori@gmail.com>
2026-05-13 03:55:20 +0530

    fix: resolve incorrect Vite plugin export name to fix Vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { TanStackStartVite } from "@tanstack/react-start/plugin/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     tsconfigPaths(),
-    TanStackStartVite({ server: { preset: "vercel" } })
+    tanstackStart({ server: { preset: "vercel" } })
   ],
 });
