f1aa590ac11f044940e849fc7066a52f4736401f
Laasya <laasyakondoori@gmail.com>
2026-05-13 04:31:16 +0530

    feat: add Vercel Edge Function adapter and vercel.json routing

--- api/index.js
+++ api/index.js
@@ -5,5 +5,5 @@ export const config = {
 import server from '../dist/server/server.js';
 
 export default async function handler(request) {
-  return server.fetch(request);
+  return await server.fetch(request);
 }
